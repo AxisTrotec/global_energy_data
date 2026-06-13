9ecfb7e1cd379db984e082d93434bd397faf02fc
Mr Axis <47964505+AxisTrotec@users.noreply.github.com>
2026-06-13 18:00:48 +0000

    Updated ingest-data.py

--- pipelines/ingest_data.py
+++ pipelines/ingest_data.py
@@ -1,117 +1,9 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[17]:
-
-
-import pandas as pd
-
-# In[18]:
-
-
-prefix = "https://github.com/AxisTrotec/global_energy_data/releases/download/data/global_power_plant_database.csv"
-
-
-# In[19]:
-
-
-dtype = {
-    "country": "string",
-    "country_long": "string",
-    "name": "string",
-    "gppd_idnr": "string",
-    "capacity_mw": "float64",
-    "latitude": "float64",
-    "longitude": "float64",
-    "primary_fuel": "string",
-    "other_fuel1": "string",
-    "other_fuel2": "string",
-    "other_fuel3": "string",
-    "commissioning_year": "float64",
-    "owner": "string",
-    "source": "string",
-    "url": "string",
-    "geolocation_source": "string",
-    "wepp_id": "string",
-    "year_of_capacity_data": "float64",
-    "generation_gwh_2013": "float64",
-    "generation_gwh_2014": "float64",
-    "generation_gwh_2015": "float64",
-    "generation_gwh_2016": "float64",
-    "generation_gwh_2017": "float64",
-    "generation_gwh_2018": "float64",
-    "generation_gwh_2019": "float64",
-    "generation_data_source": "string",
-    "estimated_generation_gwh_2013": "float64",
-    "estimated_generation_gwh_2014": "float64",
-    "estimated_generation_gwh_2015": "float64",
-    "estimated_generation_gwh_2016": "float64",
-    "estimated_generation_gwh_2017": "float64",
-    "estimated_generation_note_2013": "string",
-    "estimated_generation_note_2014": "string",
-    "estimated_generation_note_2017": "string",
-    "estimated_generation_note_2015": "string",
-    "estimated_generation_note_2016": "string"
-}
-
-
-# In[20]:
-
-
-df = pd.read_csv(prefix, sep=',', encoding='utf-8', dtype=dtype, nrows=100)
-
-
-# In[21]:
-
-
-df.head()
-
-
-# In[22]:
-
-
-df.dtypes
-
-
-# In[23]:
-
-
+from tqdm.auto import tqdm
 from sqlalchemy import create_engine
 
-
-# In[24]:
-
-
-engine = create_engine('postgresql+psycopg://{user}:{password}@{host}:{port}/{db}')
-
-
-# In[25]:
-
-
-print(pd.io.sql.get_schema(df, name='energy_data', con=engine))
-
-
-# In[26]:
-
-
-df.head(n=0).to_sql(name='energy_data', con=engine, if_exists='replace')
-
-
-# In[27]:
-
-
-df_iter = pd.read_csv(
-    prefix,
-    dtype=dtype,
-    iterator=True,
-    chunksize=100000
-)
-
-
-# In[29]:
-
-from tqdm.auto import tqdm
 import click
+import pandas as pd
 
 @click.command()
 @click.option('--pg-user', default='root', help='PostgreSQL user')
@@ -122,7 +14,73 @@ import click
 @click.option('--target-table', default='energy_data', help='Target table name')
 
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
+    prefix = "https://github.com/AxisTrotec/global_energy_data/releases/download/data/global_power_plant_database.csv"
+
     first = True
+
+    port = int(pg_port) 
+    
+    # Use the variables inside the connection string
+    connection_url = f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{port}/{pg_db}'
+    engine = create_engine(connection_url)
+    
+    dtype = {
+        "country": "string",
+        "country_long": "string",
+        "name": "string",
+        "gppd_idnr": "string",
+        "capacity_mw": "float64",
+        "latitude": "float64",
+        "longitude": "float64",
+        "primary_fuel": "string",
+        "other_fuel1": "string",
+        "other_fuel2": "string",
+        "other_fuel3": "string",
+        "commissioning_year": "float64",
+        "owner": "string",
+        "source": "string",
+        "url": "string",
+        "geolocation_source": "string",
+        "wepp_id": "string",
+        "year_of_capacity_data": "float64",
+        "generation_gwh_2013": "float64",
+        "generation_gwh_2014": "float64",
+        "generation_gwh_2015": "float64",
+        "generation_gwh_2016": "float64",
+        "generation_gwh_2017": "float64",
+        "generation_gwh_2018": "float64",
+        "generation_gwh_2019": "float64",
+        "generation_data_source": "string",
+        "estimated_generation_gwh_2013": "float64",
+        "estimated_generation_gwh_2014": "float64",
+        "estimated_generation_gwh_2015": "float64",
+        "estimated_generation_gwh_2016": "float64",
+        "estimated_generation_gwh_2017": "float64",
+        "estimated_generation_note_2013": "string",
+        "estimated_generation_note_2014": "string",
+        "estimated_generation_note_2017": "string",
+        "estimated_generation_note_2015": "string",
+        "estimated_generation_note_2016": "string"
+    }
+
+    df = pd.read_csv(prefix, sep=',', encoding='utf-8', dtype=dtype, nrows=100)
+
+
+    print(pd.io.sql.get_schema(df, name='energy_data', con=engine))
+
+    df.head()
+
+    df.dtypes
+
+    df.head(n=0).to_sql(name='energy_data', con=engine, if_exists='replace')
+
+    df_iter = pd.read_csv(
+        prefix,
+        dtype=dtype,
+        iterator=True,
+        chunksize=100000
+    )
+
 
     for df_chunk in tqdm(df_iter):
 
@@ -147,8 +105,4 @@ def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
 
 
 
-# In[ ]:
-
-
-
-
+run()
